8704f5345fdfc246f5953c66dbefb394b629e2b3
uiii <uiii.dev@gmail.com>
2013-08-29 17:42:47 +0200

    init script update
    - info messages
    - config.local.neon initialization

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+echo -n "creating src/log directory ... "
 mkdir -m 777 src/log
+echo "[✓]"
+
+echo -n "creating src/log directory ... "
 mkdir -m 777 src/temp
+echo "[✓]"
+
+echo -n "initializing config.local.neon ... "
+cp src/app/config/config.local.neon.example src/app/config/config.local.neon
+echo "[✓]"
